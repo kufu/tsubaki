724e34bc18a6d92e1d499a92e13f47c747036830
kakipo <kakipo@gmail.com>
2015-10-08 20:57:35 +0900

    Obey rubocop.

--- spec/tsubaki/validator_spec.rb
+++ spec/tsubaki/validator_spec.rb
@@ -5,29 +5,28 @@ class TestUser < TestModel
 end
 
 class StrictTestUser < TestModel
-  validates :my_number, my_number: {strict: true}
+  validates :my_number, my_number: { strict: true }
 end
 
 class TestUserAllowsNil < TestModel
-  validates :my_number, my_number: {allow_nil: true}
+  validates :my_number, my_number: { allow_nil: true }
 end
 
 class TestUserAllowsNilFalse < TestModel
-  validates :my_number, my_number: {allow_nil: false}
+  validates :my_number, my_number: { allow_nil: false }
 end
 
 class TestUserWithMessage < TestModel
-  validates :my_number, my_number: {message: 'is not looking very good!'}
+  validates :my_number, my_number: { message: 'is not looking very good!' }
 end
 
 describe Tsubaki::MyNumberValidator do
   describe 'validation' do
     context 'given the valid my numbers' do
-      [
-        '000000000000',
-        '111111111111',
-        '222222222222'
-      ].each do |num|
+      %w(
+        000000000000
+        111111111111
+        222222222222).each do |num|
         it "#{num} should be valid" do
           expect(TestUser.new(my_number: num)).to be_valid
         end
