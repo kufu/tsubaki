d8b95babcd97c17924553bb6dae162cfed59eb52
kakipo <kakipo@gmail.com>
2015-10-10 08:50:26 +0900

    Implement validation methods on MyNumber.

--- lib/tsubaki/my_number.rb
+++ lib/tsubaki/my_number.rb
@@ -1,33 +1,40 @@
 module Tsubaki
   class MyNumber
-    attr_reader :errros
+    def self.calc_check_digit(digits)
+      fail 'must be a 11 digit number' unless digits =~ /\A\d{11}\z/
 
-    def initialize(digits, options = {})
-      @errors = []
-      @digits = digits
-      @digits.gsub!(options[:divider], '') if options[:divider]
-      @errors << :invalid unless @digits =~ /\A\d{12}\z/
-    end
-
-    def valid?
-      @errors.empty?
-    end
-
-    def valid_check_digits?
-      @digits = @digits.to_s.chars.map(&:to_i)
-      return false unless @digits.length == 12
-
-      expected_check_digit = @digits.pop
-      @digits.reverse!
-      remainder = (1..11).inject(0) do |sum, n|
-        pn = @digits[n - 1]
+      arr = digits.chars.map(&:to_i).reverse!
+      rem = (1..11).inject(0) do |sum, n|
+        pn = arr[n - 1]
         qn = (n <= 6) ? (n + 1) : (n - 5)
         sum + pn * qn
       end % 11
 
-      actual_check_digit = remainder <= 1 ? 0 : (11 - remainder)
+      rem <= 1 ? 0 : (11 - rem)
+    end
 
-      actual_check_digit == expected_check_digit
+    def initialize(digits, options = {})
+      @digits = digits.to_s.freeze
+      @options = options
+    end
+
+    def valid?
+      @options[:strict] ? valid_check_digit? : valid_pattern?
+    end
+
+    def valid_pattern?
+      !!(plain_digits =~ /\A\d{12}\z/)
+    end
+
+    def valid_check_digit?
+      return false unless valid_pattern?
+      plain_digits[-1].to_i == self.class.calc_check_digit(plain_digits[0, 11]).to_i
+    end
+
+    private
+
+    def plain_digits
+      @plain_digits ||= @digits.split(@options[:divider]).join('')
     end
   end
 end
--- spec/tsubaki/my_number_spec.rb
+++ spec/tsubaki/my_number_spec.rb
@@ -1,36 +1,183 @@
 require 'spec_helper'
 
 describe Tsubaki::MyNumber do
-  describe '#valid?' do
-    subject { Tsubaki::MyNumber.new(digits, options).valid? }
-    context 'when digits contains divider & not strict mode' do
-      let(:digits) { '1111-2222-3333' }
-      let(:options) { { divider: '-', strict: false } }
-      it { is_expected.to be_truthy }
+  describe '.calc_check_digit' do
+    context 'given invalid digits' do
+      [
+        nil,
+        '12345678901X',
+        '5678-1111-9018'
+      ].each do |n|
+        describe "#{n}" do
+          it 'raises RuntimeError' do
+            expect {
+              Tsubaki::MyNumber.calc_check_digit(n)
+            }.to raise_error(RuntimeError)
+          end
+        end
+      end
+    end
+    context 'given valid digits' do
+      subject { Tsubaki::MyNumber.calc_check_digit(digits) }
+      describe '37296774233' do
+        let(:digits) { '37296774233' }
+        it { is_expected.to eq 8 }
+      end
+      describe '29217589598' do
+        let(:digits) { '29217589598' }
+        it { is_expected.to eq 2 }
+      end
     end
   end
 
-  describe '#valid_check_digits?' do
-    subject { Tsubaki::MyNumber.new(digits, {}).valid_check_digits? }
-    context 'given the valid my numbers' do
+  describe '#valid?' do
+    subject { Tsubaki::MyNumber.new(digits, options).valid? }
+    context 'when digits no options are specified' do
+      let(:options) { {} }
+      context 'given the valid my numbers' do
+        %w(
+          123456789012
+          987654321098
+          112233445566
+        ).each do |n|
+          describe "#{n}" do
+            let(:digits) { n }
+            it { is_expected.to be_truthy }
+          end
+        end
+      end
+      context 'given the invalid my numbers' do
+        [
+          nil,
+          '12345678901X',
+          '5678-1111-9018'
+        ].each do |n|
+          describe "#{n}" do
+            let(:digits) { n }
+            it { is_expected.to be_falsy }
+          end
+        end
+      end
+    end
+
+    context 'when digits contains divider & not strict mode' do
+      let(:options) { { divider: '-', strict: false } }
+      context 'given the valid my numbers' do
+        %w(
+          123456789012
+          9876-5432-1098
+          112233--445566
+        ).each do |n|
+          describe "#{n}" do
+            let(:digits) { n }
+            it { is_expected.to be_truthy }
+          end
+        end
+      end
+      context 'given the invalid my numbers' do
+        [
+          nil,
+          '0234-5678-XXXX',
+          '5678-9018'
+        ].each do |n|
+          describe "#{n}" do
+            let(:digits) { n }
+            it { is_expected.to be_falsy }
+          end
+        end
+      end
+    end
+    context 'when digits contains divider & strict mode' do
+      let(:options) { { divider: '-', strict: true } }
+      context 'given the valid my numbers' do
+        %w(
+          873321641958
+          2633-4829-1158
+          491131--223718
+        ).each do |n|
+          describe "#{n}" do
+            let(:digits) { n }
+            it { is_expected.to be_truthy }
+          end
+        end
+      end
+      context 'given the invalid my numbers' do
+        [
+          nil,
+          '0234-5678-XXXX',
+          '5678-9018',
+          '123456789012',
+          '9876-5432-1098',
+          '112233--445566'
+        ].each do |n|
+          describe "#{n}" do
+            let(:digits) { n }
+            it { is_expected.to be_falsy }
+          end
+        end
+      end
+    end
+  end
+
+  describe '#valid_pattern?' do
+    subject { Tsubaki::MyNumber.new(digits, {}).valid_pattern? }
+    context 'given the valid pattern my numbers' do
       %w(
         023456789013
         123456789018
-        123456789018).each do |n|
-        let(:digits) { n }
-        it "#{n} should be valid" do
-          is_expected.to be_truthy
+        333333333333
+      ).each do |n|
+        describe "#{n}" do
+          let(:digits) { n }
+          it 'should be valid' do
+            is_expected.to be_truthy
+          end
         end
       end
     end
     context 'given the invalid my numbers' do
       %w(
-        123456789010
-        123456789011
-        123456789012).each do |n|
-        let(:digits) { n }
-        it "#{n} should be invalid" do
-          is_expected.to be_falsy
+        12345678901
+        12345678901X
+        1234567890123
+      ).each do |n|
+        describe "#{n}" do
+          let(:digits) { n }
+          it 'should be invalid' do
+            is_expected.to be_falsy
+          end
+        end
+      end
+    end
+  end
+
+  describe '#valid_check_digit?' do
+    subject { Tsubaki::MyNumber.new(digits, {}).valid_check_digit? }
+    context 'given the valid my numbers' do
+      %w(
+        185672239885
+        176521275740
+        654629853731
+      ).each do |n|
+        describe "#{n}" do
+          let(:digits) { n }
+          it 'should be valid' do
+            is_expected.to be_truthy
+          end
+        end
+      end
+    end
+    context 'given the invalid my numbers' do
+      %w(
+        185672239886
+        176521275741
+        654629853732
+      ).each do |n|
+        describe "#{n}" do
+          let(:digits) { n }
+          it 'should be invalid' do
+            is_expected.to be_falsy
+          end
         end
       end
     end
